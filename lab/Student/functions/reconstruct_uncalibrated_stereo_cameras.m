6779d79c257078aaf1ff15760f684144b8079943
wengzehang <zehangweng@gmail.com>
2018-10-03 22:19:47 +0200

    FInish function reconstruct_uncalibrated_stereo_cameras
    finish function reconstruct_uncalibrated_stereo_cameras and past test

--- lab/Student/functions/reconstruct_uncalibrated_stereo_cameras.m
+++ lab/Student/functions/reconstruct_uncalibrated_stereo_cameras.m
@@ -16,3 +16,24 @@ function [cams, cam_centers] = reconstruct_uncalibrated_stereo_cameras( F )
 
 %------------------------------
 % TODO: FILL IN THIS PART
+    % Ma = (I|0)
+    Ma = [eye(3),[0;0;0]];
+    % Mb = (SF|h)
+    % use F to calculate the h by using F^T h = 0
+    [Utemp, Stemp, Vtemp] = svd(F');
+    h = Vtemp(:,end);
+    S = [0,-1,1;1,0,-1;-1,1,0];
+    Mb = [S*F, h];
+    
+    % to calculate ta and tb for both two cameras, using SVD again
+    % for ta
+    [a,b,c] = svd(Ma);
+    ta = c(:,end);
+    % for tb
+    [a,b,c] = svd(Mb);
+    tb = c(:,end);
+    
+    cams = zeros(3,4,2); cams(:,:,1) = Ma; cams(:,:,2) = Mb; 
+    cam_centers = zeros(4,2); cam_centers(:,1) = ta; cam_centers(:,2) = tb;
+    
+
